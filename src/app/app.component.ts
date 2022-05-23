7156fd8d1ce30041673fd3ec01aa15537de7757e
tonovarela <tonovarela@live.com>
2022-05-23 12:05:19 -0500

    se agrego la libreria qr-code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import QrScanner from 'qr-scanner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('qrvideo') video :HTMLVideoElement | undefined;
+  
+  
+  ngAfterViewInit(): void {
+    
+  }
+
+
+  async iniciarScan() {        
+    const camaras =await QrScanner.listCameras();
+    const tiene=await QrScanner.hasCamera()
+    alert(tiene);    
+  }
+
+
   title = 'qrApp';
 }
